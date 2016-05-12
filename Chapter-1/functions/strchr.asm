fa9b1355835271a9b678c1f680172573c92a9d13
shxdow <shxdow@users.noreply.github.com>
2016-05-12 22:03:34 +0200

    strchr
    strchr C function in x86 assembly.

---
+++ Chapter-1/functions/strchr.asm
@@ -0,0 +1,25 @@
+_strchr proc
+    ;__stdcall
+    push ebp
+    push edi
+    mov ebp, esp
+    mov edi, DWORD PTR [ebp+8]
+    mov edx, ds:_strlen
+    call edx
+    mov ecx, eax
+    mov eax, [ebp+0Ch]
+while:
+    scasb
+    jz found
+    dec ecx
+    jcxz proc_end
+    jz while
+    xor eax, eax
+found:
+    mov eax,DWORD PTR [edi]
+proc_end:
+    mov esp, ebp
+    pop edi
+    pop ebp
+    ret
+_strchr endp
