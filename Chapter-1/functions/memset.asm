dca749c123cd03e01f2348e724f1e2dc425640c1
shxdow <shxdow@users.noreply.github.com>
2016-05-12 19:54:48 +0200

    Update memset.asm

--- Chapter-1/functions/memset.asm
+++ Chapter-1/functions/memset.asm
@@ -1,9 +1,9 @@
 
 _memset :
-		push edi 	  			; STOSB changes the value of edi,therefore we have to save it
-		mov edi, [esp+16]	; length
-		mov ecx, [esp+12] ; byte
-		mov eax, [esp+8]	; pointer
-		rep stosb					; 
-		pop edi 					; restore the string pointer
+    push edi            ; STOSB changes the value of edi,therefore we have to save it
+    mov edi, [esp+16]   ; length
+    mov ecx, [esp+12]   ; byte
+    mov eax, [esp+8]    ; pointer
+    rep stosb           ; 
+    pop edi             ; restore the string pointer
 ret
