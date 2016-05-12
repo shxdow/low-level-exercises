6cc3a2d6c681b05e4533cf284bd24b92aa5187fd
shxdow <shxdow@users.noreply.github.com>
2016-05-12 20:58:22 +0200

    strlen
    strlen C function in x86 assembly.

--- Chapter-1/functions/strlen.asm
+++ Chapter-1/functions/strlen.asm
@@ -1,8 +1,13 @@
-; strlen(ptr * string)
-
-
 _strcmp proc
-
-
-
+    push ebp
+    mov ebp, esp
+    mov edi, [ebp+8]   ; pointer to the string
+    mov al, 0
+    mov ecx, FFFFFFFFh
+    repne scasb
+    neg ecx
+    add ecx,1
+    mov esp, ebp
+    pop ebp
+	ret
 _strcmp endp
