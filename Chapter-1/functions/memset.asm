d71887de91e67a67cbb62ea2688c7bac7735962a
shxdow <shxdow@users.noreply.github.com>
2016-05-12 19:50:49 +0200

    Update memset.asm

--- Chapter-1/functions/memset.asm
+++ Chapter-1/functions/memset.asm
@@ -1,24 +1,9 @@
-dest db "almost every programmer should know memset$"
-
-	; ##### MAIN #####
-
-	mov eax,0 ; input :: constant byte
-	mov edx,6 ; input :: length
-	
-	mov edi,dest ; assigns a pointer to the first character of the string
-	push edx  ;     constant byte
-	push eax  ;	length
-	call _memset
-
-ret
 
 _memset :
-	push edi 	  ; STOSB changes the value of edi,therefore we have to save it
-	
-	mov edi, [esp+8] ; string pointer 32 bit that points to the data segment, the string is not saved in the stack
-	mov ecx, [esp+16]  ; length
-	mov eax, [esp+12] ; constant byte
-	rep stosb
-
-	pop edi ; restore the string pointer
+		push edi 	  			; STOSB changes the value of edi,therefore we have to save it
+		mov edi, [esp+16]	; length
+		mov ecx, [esp+12] ; byte
+		mov eax, [esp+8]	; pointer
+		rep stosb					; 
+		pop edi 					; restore the string pointer
 ret
