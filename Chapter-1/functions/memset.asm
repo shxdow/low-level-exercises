7be7e31c762193287cb9d63836a8a264c1d71ff7
shxdow <shxdow@users.noreply.github.com>
2016-05-12 19:44:39 +0200

    Update memset.asm

--- Chapter-1/functions/memset.asm
+++ Chapter-1/functions/memset.asm
@@ -1,19 +1,3 @@
-; Version 1.0  ------------------------------------------------------------
-;
-;	The function doesn't initialize registers.
-;	It sets up a stack frame for no reasons.
-;	It stores untouched registers.
-;	It does not take into account the return value.
-;	Stack offsets were all off by 4 bytes, I forgot that 'call' pushes EIP.
-
-; Version 2.0  ------------------------------------------------------------
-;
-;
-;
-;
-;
-; ############################################################ MEMSET ############################################################
-
 dest db "almost every programmer should know memset$"
 
 	; ##### MAIN #####
@@ -22,18 +6,18 @@ dest db "almost every programmer should know memset$"
 	mov edx,6 ; input :: length
 	
 	mov edi,dest ; assigns a pointer to the first character of the string
-	push eax  ; constant byte
-	push edx  ;	length
+	push edx  ;     constant byte
+	push eax  ;	length
 	call _memset
 
 ret
 
 _memset :
-	push edi ; STOSB changes the value of edi,therefore we have to save it
+	push edi 	  ; STOSB changes the value of edi,therefore we have to save it
 	
-	mov edi, [esp+16] ; string pointer 32 bit that points to the data segment, the string is not saved in the stack
-	mov ecx, [esp+8]  ; length
-	mov eax, [ebp+12] ; constant byte
+	mov edi, [esp+8] ; string pointer 32 bit that points to the data segment, the string is not saved in the stack
+	mov ecx, [esp+16]  ; length
+	mov eax, [esp+12] ; constant byte
 	rep stosb
 
 	pop edi ; restore the string pointer
