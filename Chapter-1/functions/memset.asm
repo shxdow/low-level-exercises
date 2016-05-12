91d24fa4c0d5438370c709f36db7237b89631f17
shxdow <shxdow@users.noreply.github.com>
2016-05-12 21:02:41 +0200

    memset
    memset C function in x86 assembly.

--- Chapter-1/functions/memset.asm
+++ Chapter-1/functions/memset.asm
@@ -1,8 +1,11 @@
 _memset :
+    push ebp
     push edi            ; STOSB changes the value of edi,therefore we have to save it
-    mov edi, [esp+16]   ; length
-    mov ecx, [esp+12]   ; byte
-    mov eax, [esp+8]    ; pointer
+    mov edi, [ebp+16]   ; length
+    mov ecx, [ebp+12]   ; byte
+    mov eax, [ebp+8]    ; pointer
     rep stosb           ; 
+    mov ebp, esp
+    pop ebp
     pop edi             ; restore the string pointer
 ret
