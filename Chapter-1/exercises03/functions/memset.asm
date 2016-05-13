57946a171d49d294c5b29b00895eb920e8611112
shxdow <shxdow@users.noreply.github.com>
2016-05-13 15:19:43 +0200

    .
    .

--- Chapter-1/exercises03/functions/memset.asm
+++ Chapter-1/exercises03/functions/memset.asm
@@ -1,9 +1,9 @@
 _memset :
     push ebp
     push edi            ; STOSB changes the value of edi,therefore we have to save it
-    mov edi, [ebp+16]   ; length
-    mov ecx, [ebp+12]   ; byte
-    mov eax, [ebp+8]    ; pointer
+    mov ecx, [ebp+10h]  ; length
+    mov al, [ebp+Ch]    ; byte
+    mov edi, [ebp+8]    ; pointer
     rep stosb           ; 
     mov ebp, esp
     pop ebp
