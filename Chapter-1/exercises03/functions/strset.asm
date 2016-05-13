24730a6ad40052d519a05468350d879d3c7fd4d7
shxdow <shxdow@users.noreply.github.com>
2016-05-13 15:24:15 +0200

    .
    .

--- Chapter-1/exercises03/functions/strset.asm
+++ Chapter-1/exercises03/functions/strset.asm
@@ -1,11 +1,11 @@
-_memset :
+_strset proc
     push ebp
     push edi            ; STOSB changes the value of edi,therefore we have to save it
-    mov edi, [ebp+16]   ; length
-    mov ecx, [ebp+12]   ; byte
+    mov edi, [ebp+Dh]   ; length
+    mov ecx, [ebp+Ch]   ; char
     mov eax, [ebp+8]    ; pointer
     rep stosb
     mov ebp, esp
     pop ebp
     pop edi             ; restore the string pointer
-ret
+_strset endp
