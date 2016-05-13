28bad76ac951d0b2c47e7512aeb739a4b6d69422
shxdow <shxdow@users.noreply.github.com>
2016-05-13 14:55:49 +0200

    .
    .

--- Chapter-1/exercises01/ex01.asm
+++ Chapter-1/exercises01/ex01.asm
@@ -1,11 +1,12 @@
-1. This function uses a combination SCAS and STOS to do its work. First, explain what is the type of the [EBP+8] and [EBP+C] in line 1 and 8, respectively. Next, explain what this snippet does.
+1. This function uses a combination SCAS and STOS to do its work. First, explain what is the type of the [EBP+8] and [EBP+C] in
+   line 1 and 8, respectively. Next, explain what this snippet does.
 
 01: 8B 7D 08         mov   edi, [ebp+8]     ;   edi = first argument
-02: 8B D7            mov   edx, edi         ;
+02: 8B D7            mov   edx, edi         ;   stores the string pointer,as the function will modify it
 03: 33 C0            xor   eax, eax         ;   sets eax to 0
 04: 83 C9 FF         or    ecx, 0FFFFFFFFh  ;   sets ecx to -1
 05: F2 AE            repne scasb            ;   repeats if ZF = 0, max ecx times. Compares a byte from eax with the one pointed by edi
-06: 83 C1 02         add   ecx, 2           ;   
+06: 83 C1 02         add   ecx, 2           ;   ecx was initialized to -1,and strlen( ) does not count the NULL byte (we have to subtract 1 once more)
 07: F7 D9            neg   ecx              ;   ecx is a negative number
 08: 8A 45 0C         mov   al, [ebp+0Ch]    ;   al = second argument
 09: 8B FA            mov   edi, edx         ;   edi = first argument
--- Chapter-1/exercises03/functions/strlen.asm
+++ Chapter-1/exercises03/functions/strlen.asm
@@ -1,12 +1,12 @@
 _strlen proc
     push ebp
     mov ebp, esp
-    mov edi, [ebp+8]   ; pointer to the string
+    mov edi, [ebp+8]
     mov al, 0
     mov ecx, FFFFFFFFh
     repne scasb
     neg ecx
-    add ecx,1
+    add ecx,2
     mov eax, ecx
     mov esp, ebp
     pop ebp
