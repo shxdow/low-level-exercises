ac7a3a1de25a039cbc9a2db64958c020ae82d568
shxdow <thetest1@hotmail.it>
2016-10-12 18:34:28 +0200

    Date:      Wed Oct 12 18:34:28 2016 +0200 Changes to be committed: 	modified:   Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeInitializeDpc.asm 	modified:   Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread.asm KeReadyThread: found the data structure used, it seems that Windows' data structure "generally" look something like this _K<name>. I don't get what _LIST_ENTRY and _SINGLE_LIST_ENTRY are.

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeInitializeDpc.asm
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeInitializeDpc.asm
@@ -5,27 +5,41 @@ nt!KeInitializeDpc:
 828ff03c 8bec            mov     ebp,esp
 828ff03e 8b4508          mov     eax,dword ptr [ebp+8]   ; eax = & Dpc
 828ff041 33c9            xor     ecx,ecx			     ; ecx = 0
-828ff043 83601c00        and     dword ptr [eax+1Ch],0   ; SystemArgument1 = 0
+828ff043 83601c00        and     dword ptr [eax+1Ch],0   ; Dpc.DpcData = 0
 828ff047 c60013          mov     byte ptr [eax],13h	     ; Dpc.Type = 19
 828ff04a c6400101        mov     byte ptr [eax+1],1	     ; Dpc.Importance = 1
 828ff04e 66894802        mov     word ptr [eax+2],cx   	 ; Dpc.Number = 0
-828ff052 8b4d0c          mov     ecx,dword ptr [ebp+0Ch] ; ecx = &Dcp.ListEntry
-828ff055 89480c          mov     dword ptr [eax+0Ch],ecx ; Dpc.DpcListEntry = ecx // It's now a pointer
-828ff058 8b4d10          mov     ecx,dword ptr [ebp+10h] ; ecx = Dpc.Number
-828ff05b 894810          mov     dword ptr [eax+10h],ecx ; Dpc.Number = ecx		  // Same ^
+828ff052 8b4d0c          mov     ecx,dword ptr [ebp+0Ch] ; ecx = DeferredRoutine
+828ff055 89480c          mov     dword ptr [eax+0Ch],ecx ; Dpc.DeferredRoutine = DeferredContext
+828ff058 8b4d10          mov     ecx,dword ptr [ebp+10h] ; ecx = DeferredContext
+828ff05b 894810          mov     dword ptr [eax+10h],ecx ; Dpc.DeferredContext = DeferredContext
 828ff05e 5d              pop     ebp
 828ff05f c20c00          ret     0Ch
 
 
-typedef struct _KDPC
-{
-     UCHAR Type;				// ebp+8
-     UCHAR Importance;			// ebp+9 
-     WORD Number;				// ebp+10
-     LIST_ENTRY DpcListEntry;	// ebp+12
-     PVOID DeferredRoutine;		// ebp+20
-     PVOID DeferredContext;		// ebp+24
-     PVOID SystemArgument1;		// ebp+28
-     PVOID SystemArgument2;		// ebp+32
-     PVOID DpcData;				// ebp+36
-} KDPC, *PKDPC;
+lkd> dt _KDPC
+nt!_KDPC
+   +0x000 Type             : UChar
+   +0x001 Importance       : UChar
+   +0x002 Number           : Uint2B
+   +0x004 DpcListEntry     : _LIST_ENTRY
+   +0x00c DeferredRoutine  : Ptr32     void
+   +0x010 DeferredContext  : Ptr32 Void
+   +0x014 SystemArgument1  : Ptr32 Void
+   +0x018 SystemArgument2  : Ptr32 Void
+   +0x01c DpcData          : Ptr32 Void	
+
+C code
+
+VOID KeInitializeDpc(
+  _Out_    PRKDPC             Dpc,
+  _In_     PKDEFERRED_ROUTINE DeferredRoutine,
+  _In_opt_ PVOID              DeferredContext) {
+  
+	Dpc.Data = 0;
+	Dpc.Type = 0x13;
+	Dpc.Importance = 1;
+	Dpc.Number = 0;
+	Dpc->DeferredRoutine = DeferredRoutine;
+	Dpc->DeferredContext = DeferredContext;
+}
