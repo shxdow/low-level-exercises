b6e1ea2b424721510ce6a457f4a94fa2e110ddb7
shxdow <shxdow@users.noreply.github.com>
2016-10-13 17:54:32 +0200

    Cleaned the code

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread.asm
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread.asm
@@ -1,11 +1,11 @@
 lkd> uf KeReadyThread
 nt!KeReadyThread:
 829080f6 8bff            mov     edi,edi
-829080f8 56              push    esi									; ebp
-829080f9 8bf0            mov     esi,eax								; .
-829080fb 8b4650          mov     eax,dword ptr [esi+50h]				; .
-829080fe 8b4874          mov     ecx,dword ptr [eax+74h]				; .
-82908101 f6c107          test    cl,7									; .
+829080f8 56              push    esi
+829080f9 8bf0            mov     esi,eax							
+829080fb 8b4650          mov     eax,dword ptr [esi+50h]
+829080fe 8b4874          mov     ecx,dword ptr [eax+74h]
+82908101 f6c107          test    cl,7
 82908104 7409            je      nt!KeReadyThread+0x19 (8290810f)
 
 nt!KeReadyThread+0x10:
