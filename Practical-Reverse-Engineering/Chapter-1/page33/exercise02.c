7fc8898cdbd281179fe430404b1be845e3f4d88f
shxdow <thetest1@hotmail.it>
2016-10-14 17:02:54 +0200

    Storin IDTR in a variable correctly

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise02.c
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise02.c
@@ -1,10 +1,12 @@
 #include <windows.h>        // windows API
-#include <tlhelp32.h>       // 
-#include <tchar.h>          // 
+// #include <tlhelp32.h>
+// #include <tchar.h>
+// #include <intrin.h>
+#include <iostream>
 
-struct IDT{
-	DWORD64 base;
+struct IDTR{		// struct rappresenting IDT register
 	WORD  limit;
+	DWORD64 base;
 };
 
 BOOL __stdcall DllMain(HINSTANCE hinstDLL, DWORD fdwReason,LPVOID lpvReserved);
@@ -21,17 +23,18 @@ int main()
 
 BOOL __stdcall DllMain(HINSTANCE hinstDLL, DWORD fdwReason,LPVOID lpvReserved){
     
-    IDT idtRetrieved;
-    // PROCESSENTRY32 process;
-    // short ret;
-    // DWORD tmp
+	IDTR idt;
+	// PROCESSENTRY32 process;
+    	__asm__ volatile( "sidt %0" : "=m" (idt) ); 
+  
+	std::cout << "Limit:\t" << idt.limit << "\n";
+	std::cout << "Base:\t" << std::hex << idt.base << std::endl;
+	
+	if(0x8003F400 < idt.limit && idt.limit > 0x80047400){
+		std::cout << "ret 0" << std::endl;
+        	return 0;
+	}
 
-    asm("movl %%esp %0" : "=r" (idtRetrieved) );
-   
-
-/*    if(8003F400 < idt && idt > 80047400h){
-        return 0;
-    }*/
     
     // ret = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
     
