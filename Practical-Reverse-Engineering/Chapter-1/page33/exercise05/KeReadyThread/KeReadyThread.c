21f44434ff52e542c0e21e34467269dd68dc8fcf
shxdow <shxdow@users.noreply.github.com>
2016-10-22 23:51:47 +0200

    Deleted the file

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread/KeReadyThread.c
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread/KeReadyThread.c
@@ -1,12 +1,5 @@
+#include
+#include 
+#include
 
-// #include <>
-
-Type KeReadyThread(KTHREAD thread) // what type is it
-{
-	if(thread.<some field> == 7)
-	{
-		KiFastReadyThread
-		return ;
-	}
-
-}
+//
