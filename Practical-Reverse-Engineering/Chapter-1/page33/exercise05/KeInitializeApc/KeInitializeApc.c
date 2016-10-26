f148433c1029cf6f42439e5ec420ee74173455be
shxdow <shxdow@users.noreply.github.com>
2016-10-26 19:49:17 +0200

    I still have to figure out what happens in the if statement.Aside from that I'm close to finishing.

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeInitializeApc/KeInitializeApc.c
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeInitializeApc/KeInitializeApc.c
@@ -1,12 +1,9 @@
 
-
-
-
 NTKERNELAPI VOID KeInitializeApc (
 	
-	IN PRKAPC Apc,
+	IN PRKAPC Apc,			// eax = ebp+8
 	
-	IN PKTHREAD Thread,
+	IN PKTHREAD Thread,		// 
 	
 	IN KAPC_ENVIRONMENT Environment,
 	
@@ -22,15 +19,41 @@ NTKERNELAPI VOID KeInitializeApc (
 
 )
 {
+	BYTE var;
 	Apc.Type = 0x12
 	Apc.Size = 0x30
 
+	// line 12
 	if (Apc.Size == 2)
 	{
-		// Thread.ApcStateIndex saved somewhere
+		// line 15
+		var = Thread.UserIdealProcessor;
 	}
+	
+	// line 18
+	Environment = Apc;
 
-	Environment. = ecx ?	
+	// line 19 - 20
+	NormalRoutine = KernelRoutine;
+
+	// line 21 - 22
+	ApcMode = RundownRoutine;
+
+	// line 23 - 24
+	NormalContext = NormalRoutine;
+
+	// line 28
+	if (NormalRoutine == 0) // 0 is for kernel APC's and 1 for user ones.
+	{
+		// line 38 - 39
+		// = NormalRoutine;		
+	} else {
+		// line 31 - 34
+		// d = var;
+	}
+}
+
+
+
 	
 
-}
