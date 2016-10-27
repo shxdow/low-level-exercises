16b047e6f4c7f6a7a32d463cb8e722782fec842e
shxdow <thetest1@hotmail.it>
2016-10-27 18:26:19 +0200

    Completed.How am I supposed to know whether arguments are pointers or not

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KiInitializeTSS/KiInitializeTSS.asm
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KiInitializeTSS/KiInitializeTSS.asm
@@ -1,12 +1,12 @@
 kd> uf KiInitializeTSS
 nt!KiInitializeTSS:
-81a16eea 8bff            mov     edi,edi
-81a16eec 55              push    ebp
-81a16eed 8bec            mov     ebp,esp
-81a16eef 8b4508          mov     eax,dword ptr [ebp+8]
-81a16ef2 6683606400      and     word ptr [eax+64h],0
-81a16ef7 6683606000      and     word ptr [eax+60h],0
-81a16efc 66c74066ac20    mov     word ptr [eax+66h],20ACh
-81a16f02 66c740081000    mov     word ptr [eax+8],10h
-81a16f08 5d              pop     ebp
-81a16f09 c20400          ret     4
+804ff400 8bff            mov     edi,edi
+804ff402 55              push    ebp
+804ff403 8bec            mov     ebp,esp
+804ff405 8b4508          mov     eax,dword ptr [ebp+8]
+804ff408 6683606400      and     word ptr [eax+64h],0
+804ff40d 6683606000      and     word ptr [eax+60h],0
+804ff412 66c74066ac20    mov     word ptr [eax+66h],20ACh
+804ff418 66c740081000    mov     word ptr [eax+8],10h
+804ff41e 5d              pop     ebp
+804ff41f c20400          ret     4
