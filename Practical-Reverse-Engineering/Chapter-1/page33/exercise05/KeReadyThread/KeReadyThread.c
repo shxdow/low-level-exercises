b884dcb020955df9fe4658a7790bdac0b9b3fdf2
shxdow <thetest1@hotmail.it>
2016-10-28 22:29:53 +0200

    Wrote fnc calls.Argumetns still have to be figured out.

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread/KeReadyThread.c
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread/KeReadyThread.c
@@ -1,5 +1,11 @@
-#include
-#include 
-#include
-
-//
+VOID NTAPI KeReadyThread(PKTHREAD Thread)
+{
+	// The KeRaiseIrqlToDpcLevel routine raises the hardware priority to 
+	// IRQL = DISPATCH_LEVEL, thereby masking off interrupts of equivalent or
+	// lower IRQL on the current processor.
+	KIRQL IRQL = KeRaiseIrqlToDpcLevel(void);
+	
+	KiReadyThread();
+	KiUnlockDispatcherDatabase();
+	return ;
+}
