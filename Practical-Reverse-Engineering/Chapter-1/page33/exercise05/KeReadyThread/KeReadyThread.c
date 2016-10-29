7ac2d0feef93657024dcf9b33c6b4821448e8fca
shxdow <shxdow@users.noreply.github.com>
2016-10-29 15:55:39 +0200

    Completed. It should include everything.

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread/KeReadyThread.c
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeReadyThread/KeReadyThread.c
@@ -3,9 +3,17 @@ VOID NTAPI KeReadyThread(PKTHREAD Thread)
 	// The KeRaiseIrqlToDpcLevel routine raises the hardware priority to 
 	// IRQL = DISPATCH_LEVEL, thereby masking off interrupts of equivalent or
 	// lower IRQL on the current processor.
-	KIRQL IRQL = KeRaiseIrqlToDpcLevel(void);
+	KIRQL irql = KeRaiseIrqlToDpcLevel();
 	
-	KiReadyThread();
+	KiReadyThread(Thread);
+	/*
+	 * The Windows scheduling code is implemented in the kernel.
+	 * There’s no single “scheduler” module or routine, however—the code is spread
+	 * throughout the kernel in which scheduling-related events occur.
+	 * The routines that perform these duties are collectively called the kernel’s dispatcher
+	 */
+
 	KiUnlockDispatcherDatabase();
+
 	return ;
 }
