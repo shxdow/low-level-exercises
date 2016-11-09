92449d729a2c915f7ad921fb1cfe0cb902e5318d
shxdow <thetest1@hotmail.it>
2016-11-09 21:00:30 +0100

    CH01-ex06 Decompiled sub_13842 to C
    The solution provided by github.com/baderj differs from mine,as it uses
    many local variables which are clearly not present in the binary; and I
    believe his solution to be wrong.

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise06/sub_13842.c
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise06/sub_13842.c
@@ -1,37 +1,26 @@
-struct s1
+struct struct_arg1
 {
-	DWORD var1;// 4 bytes
-	DWORD var2;// 4 bytes
-	DWORD var3;// 4 bytes
-	DWORD var4;// 4 bytes
-	DWORD var5;// 4 bytes
-	DWORD var6;// 4 bytes
-	DWORD var7;// 4 bytes
-	DWORD var8;// 4 bytes
-	DWORD var9;// 4 bytes
-	DWORD var10;// 4 bytes
-	DWORD var11;// 4 bytes
-	DWORD var12;// 4 bytes
-	DWORD var13;// 4 bytes
-	DWORD var14;// 4 bytes
-	
-	
+	/* 0x22 bytes of stuff */
+	BYTE member0;
+	/* 0x3D bytes of some unknown fields */
+	struct_arg1 * member1;
+	/* 0x14 bytes of some unknown fields */
+	struct_arg2 * member2;
 }
 
-/* I can tell where members are as long as there's sth before / after them */
-
-/* the calle has 4 arguments,so they may have sth to do */
-unknown_type __fastcall sub_13842(s1 xxx,previous_arg_4)
+struct struct_arg2
 {
-	xxx.var1 -= 0x24;
-	xxx.var14--;
-	
-	(* unknown_fnc)(previous_arg_4,ecx);
+	/* 8 bytes of stuff */
+	DWORD something;
+	/* maybe there's something else */
 }
 
 
-/*
-	sub_11798:
-			contains the fnc call + eax
-	
-*/
+unknown_type __fastcall sub_13842(struct_arg1 arg_1, struct_arg2 arg_2)
+{
+	arg_1.member0 -= 1;
+	arg_1->member1 -= 0x24;
+	arg_1->member2 = arg_2;
+
+	(* unknown_fnc)(arg_2 ,arg_1);
+}
