f35cf2b41bb4a8eda43a601a1c754b1bffe1f51a
shxdow <shxdow@users.noreply.github.com>
2016-11-13 00:48:27 +0100

    KeInitializeDpc: Moved C code to a dedicated file
    title.

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeInitializeDpc/KeInitializeDpc.asm
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise05/KeInitializeDpc/KeInitializeDpc.asm
@@ -28,18 +28,3 @@ nt!_KDPC
    +0x014 SystemArgument1  : Ptr32 Void
    +0x018 SystemArgument2  : Ptr32 Void
    +0x01c DpcData          : Ptr32 Void	
-
-C code
-
-VOID KeInitializeDpc(
-  _Out_    PRKDPC             Dpc,
-  _In_     PKDEFERRED_ROUTINE DeferredRoutine,
-  _In_opt_ PVOID              DeferredContext) {
-  
-	Dpc.Data = 0;
-	Dpc.Type = 0x13;
-	Dpc.Importance = 1;
-	Dpc.Number = 0;
-	Dpc->DeferredRoutine = DeferredRoutine;
-	Dpc->DeferredContext = DeferredContext;
-}
