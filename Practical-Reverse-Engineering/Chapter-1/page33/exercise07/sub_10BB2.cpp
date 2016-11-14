18be5c41ee1ae54e39a8bf5894d7717c16b2a662
shxdow <shxdow@users.noreply.github.com>
2016-11-14 21:38:53 +0100

    CH01-ex07 Finished and added a function type
    Mostly went around and either completed missing parts regarding weird
    fields or substitued immediate values w/ corresponding sizeof()'s.

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise07/sub_10BB2.cpp
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise07/sub_10BB2.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-<unknown_type> __stdcall sub_10BB2(char * pe_file)
+_IMAGE_SECTION_HEADER __stdcall * sub_10BB2(char * pe_file)
 {	
 	/* Get a pointer to the beginning of the DOS_HEADER */
 	IMAGE_DOS_HEADER * dos_header = (IMAGE_DOS_HEADER * ) pe_file;
@@ -8,23 +8,30 @@
 	/* Get a pointer to the beginning of the PE_HEADER */
 	_IMAGE_NT_HEADERS * pe_header = (_IMAGE_NT_HEADERS *) dos_header->e_lfanew;
 	
-	/* Get a pointer to the beginning of the SECTION_HEADER */
-	_IMAGE_SECTION_HEADER * section_header = pe_header + ;
+	/* Get PE Optional header size */
+	unsigned __int8 opt_header_size = pe_header->FileHeader.SizeOfOptionalHeader; 
 	
-	if (pe_header->FileHeader.SizeOfOptionalHeader <= 0)
+	/* Get a pointer to the beginning of the SECTION_HEADER */
+	_IMAGE_SECTION_HEADER * section_header = pe_header + \
+											 sizeof(_IMAGE_NT_HEADERS) + \
+											 opt_header_size;
+	
+	/* Get a pointer to the beginning of the SECTION_HEADER */
+	if (pe_header->FileHeader.NumberOfSections <= 0)
 	{
 		return NULL;
 	}
 	
 	/* Loop looking for something */
-	for (int i = 0;i < arg_1->. ;++i)
+	for (int i = 0;i < pe_header->FileHeader.NumberOfSections ;++i)
 	{
-		if (dword_169A4(, <something else>) == 0)
+		if (dword_169A4(section_header, <something else>) == 0)
 		{
-			we found <whatever we're looking for>
-			return ;
+			/* We found <whatever we're looking for> */)
+			return section_header;
 		} else {
-			
+			/* Increase the section pointer by sizeof(section_header) */
+			section_header += sizeof(_IMAGE_SECTION_HEADER);
 		}
 	}
 }
