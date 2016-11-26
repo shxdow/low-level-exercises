47d0657fb712c6d29687275826479d2682ebd625
shxdow <shxdow@users.noreply.github.com>
2016-11-26 17:37:17 +0100

    Trying to decompile back to C mistery1
    As far as I can tell single bytes are being addressed (maybe the
    argument is a char)

--- Practical-Reverse-Engineering/Chapter-2/exercise01/mistery1.c
+++ Practical-Reverse-Engineering/Chapter-2/exercise01/mistery1.c
@@ -1,51 +1,14 @@
-/* ARM code
- * R4-R8 are saved
- *
- * R6: should be a variable or sth
- *
- *
- */
 
-unknown_type sub_1234(int32_t arg0, arg1, arg2, int32_t arg3)
+
+type mistery1(int8_t * arg0, arg1)
 {
-		// line 4
-		if (arg4 == 0x2D) {
-    			arg0.field0_c = R3;
-	    		R6 = 1;
-		} else {
-		    	// line 6
-			    if (arg4 == 0x2B) {
-				        R6 = 0;
-			    }
-		}
-		// line 10
-		if (arg4 == 0x30) {
-				// line 12
-				arg4 = arg0 + 1;
-				do {
-						arg0 = arg4;
+    // line 2: save registers R4 - R8
+    if (*(arg0) == 0x2D) {
+        *(++arg0) = *(arg0);
+        return 1;
+    }
+    if (*(arg0) <= 0x2B) {
+        *(++arg0) = *(arg0);
+    }
 
-						++arg3;
-				} while (arg3 == 0x30);
-		}
-		// line 18 - 22 initialize some registers
-		R12 = 0;
-		R4 = 0;
-		R5 = 0;
-		R8 = 0xA;
-
-		// all registers are set to R7 (it may be 0)
-		// line 27 - 32 (some calculations are done)
-		if (R7 == 9) {
-			    // line 46
-			    if (arg2 >= 0x80000000) {
-                        return 0;
-                } else {
-                        // line 49
-                        if (R6 != 0) {
-                            // line 51 - 52 set some regs to zero
-                        }
-				        R4 = *arg2;
-			    		return 1;
-			    }
-		}
+}
