551546dd34daf1c4e70b7cb3519aa55f547decb6
shxdow <shxdow@users.noreply.github.com>
2016-12-11 11:48:43 +0100

    Solved mistery4

--- Practical-Reverse-Engineering/Chapter-2/exercise04/mistery4.c
+++ Practical-Reverse-Engineering/Chapter-2/exercise04/mistery4.c
@@ -3,11 +3,11 @@
  *  this function checks if the argument is 0,if not sets it.
  *  the type of arg0 is uncertain.
  */
-pointer_type mistery4 (pointer_type * arg0)
+int mistery4 (int * arg0)
 {
-    if (arg0 != 0)
-    {   // what's the point of this line
-        return *(arg0 - 0x8);
+    if (arg0 == 0)
+    {
+        return 0;
     }
-    return 0;
+    return *(arg0 - 0x8);
 }
