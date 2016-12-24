b121d6487caf98e2791ce7faa6da1b73fb6ac261
shxdow <shxdow@users.noreply.github.com>
2016-12-24 17:10:14 +0100

    Fixed addresses

--- Practical-Reverse-Engineering/Chapter-1/page33/exercise04/memset.asm
+++ Practical-Reverse-Engineering/Chapter-1/page33/exercise04/memset.asm
@@ -1,11 +1,11 @@
 _memset :
     push ebp
     push edi            ; STOSB changes the value of edi,therefore we have to save it
-    mov edi, [ebp+16]   ; length
-    mov ecx, [ebp+12]   ; byte
-    mov eax, [ebp+8]    ; pointer
+    mov edi, [ebp+8]   	; set edi to the string pointer
+    mov ecx, [ebp+0xC]  ; set ecx to the counter
+    mov eax, [ebp+0x10] ; set eax to the byte we want to write
     rep stosb
     mov ebp, esp
-    pop ebp
-    pop edi             ; restore the string pointer
+    pop edi
+    pop ebp             ; restore the string pointer
 ret
