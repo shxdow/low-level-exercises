bdf3d05d7df0ab4408ba04200f2acd20f2fde0a8
shxdow <dysonfrost@protonmail.ch>
2017-08-22 23:31:15 +0200

    Add scull.c
    As the the structure of the project got completely revisited in commit
    c19c9fe, scull.c got rewrote.

---
+++ linux-device-drivers/scull.c
@@ -0,0 +1,84 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/fs.h>
+#include <linux/cdev.h> 
+
+/* driver numbers */
+static int scull_major;
+static int scull_minor;
+static dev_t dev;
+static unsigned int scull_nr_devs = 1;
+
+/* error checking */
+static int result;
+
+/* the kernel uses structures of type struct cdev to represent char */
+/* devices internally. */
+struct cdev *krn_cdev;
+/* this struct contains all the methods that the driver exposes */
+struct file_operations *fops;
+
+/* driver's own representation of himself */
+struct scull_dev {
+    struct scull_qset *data;  /* Pointer to first quantum set */
+    int quantum;              /* the current quantum size */
+    int qset;                 /* the current array size */
+    unsigned long size;       /* amount of data stored here */
+    unsigned int access_key;  /* used by sculluid and scullpriv */
+    struct semaphore sem;     /* mutual exclusion semaphore     */
+    struct cdev cdev;         /* Char device structure      */
+};
+
+module_param(scull_major, int, S_IRUGO);
+
+static void scull_setup_cdev(struct cdev *cdev, ) {
+    cdev_init(krn_cdev, fops);
+}
+
+static int init_scull(void)
+{
+    /* assign major number or request it dynamically */
+    if (scull_major) {
+        /* given two integer numbers we can combine them and convert */
+        /* them into one dev_t using the macro MKDEV. */
+        dev = MKDEV(scull_major, scull_minor);
+        /* register a range of device numbers */
+        result = register_chrdev_region(dev, scull_nr_devs, "scull");
+    } else {
+        result = alloc_chrdev_region(&dev,
+                                     scull_minor, scull_nr_devs, "scull");
+        scull_major = MAJOR(dev);
+    }
+
+    if (result < 0) {
+        printk(KERN_WARNING "scull: can't get major %d\n", scull_major);
+        return result;
+    }
+
+    /* Before the kernel invokes your device’s operations, you must allocate */
+    /* and register one or more of these structures. */
+    cdev_init(krn_cdev, fops);
+
+    /* We can now expose our drive to the system */
+    cdev_add(krn_cdev, scull_major, 1);
+
+    return 0;
+
+}
+
+static void cleanup_scull(void)
+{
+    /* free device number */
+    unregister_chrdev_region(dev, 1);
+
+    /* unregister driver from system aka no longer accessible */
+    cdev_del(krn_cdev);
+
+    return ;
+}
+
+/* this has to be uppercase, else the module won't compile */
+MODULE_LICENSE("GPL");
+module_init(init_scull);
+module_exit(cleanup_scull);
